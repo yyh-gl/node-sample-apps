14bba5a819c220109b4193d7ce1732e211c37733
yyh-gl <yhonda.95.gl@gmail.com>
2018-05-13 15:49:56 +0900

    Viewを追加

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 var express = require("express");
 var app = express();
 
+// View EngineにEJSを指定。
+app.set('view engine', 'ejs');
+
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
 var server = app.listen(3000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
 /* 3. 以後、アプリケーション固有の処理 */
+
+// "/"へのGETリクエストでindex.ejsを表示する。
+// 拡張子（.ejs）は省略されていることに注意。
+app.get("/", function(req, res, next){
+    res.render("index", {});
+});
 
 // 写真のサンプルデータ
 var photoList = [
