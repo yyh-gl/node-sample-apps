c66b427658b67f91c0600013e4bc5db973b8665c
yyh-gl <yhonda.95.gl@gmail.com>
2018-05-14 11:04:36 +0900

    2つ目のサンプルプログラム完成

---
+++ sample2/README.md
@@ -0,0 +1,1 @@
+https://qiita.com/tamura_CD/items/e3abdab9b8c5aa35fa6b
--- sample2/routes/samples.js
+++ sample2/routes/samples.js
@@ -21,4 +21,25 @@ router.get('/hello', function(req, res, next) {
     res.send(param);
 });
 
+/* サンプルAPI③
+ * http://localhost:3000/samples/hello/(任意の文字列) にGETメソッドのリクエストを投げると、
+ * JSON形式で(任意の文字列)を返す。
+ */
+router.get('/hello/:place', function (req, res, next) {
+    // var param = {"result":"Hello "+ req.params.place + " !"};                          // ← ★★ 削除 ★★
+    var param = {"result":"Hello "+ req.params.place + " !","shop name":req.query.shop};  // ← ★★ 追加 ★★
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.send(param);
+});
+
+/* サンプルAPI④
+ * http://localhost:3000/samples にPOSTメソッドのリクエストを投げると、
+ * JSON形式で文字列を返す。
+ */
+router.post('/', function(req, res, next) {
+    var param = {"値":"POSTメソッドのリクエストを受け付けました","bodyの値":req.body.card};
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.send(param);
+});
+
 module.exports = router;
